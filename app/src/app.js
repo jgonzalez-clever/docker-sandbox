8a19fe325d3e80da27d769a3e135a1914574b943
santiigerace <sgerace@cleveritgroup.com>
2024-01-08 15:09:40 -0300

    primercommit

--- app/src/app.js
+++ app/src/app.js
@@ -2,7 +2,7 @@ import http from "http"
 
 const server = http.createServer((request, response) => {
     console.log('somebody make a request')
-    response.end('response for you! hello word')
+    response.end('response for you! Hello world!!')
 })
 
 const connectedServer = server.listen(8080, () => {
